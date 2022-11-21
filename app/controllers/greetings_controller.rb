fd63c44a18942b70a57aafddfcebf44247da06fa
Abdullah2213565 <dulakhan024@gmail.com>
2022-11-21 11:24:13 +0100

    Greeting controller return json

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,5 +1,10 @@
 class GreetingsController < ApplicationController
   def index
     @greetings = Greeting.all
+    @greeting = @greetings.sample
+
+    respond_to do |format|      
+      format.json { render json: @greeting }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'greetings/index'
   root 'root#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    resources :greetings, only: [:index]
+  end
 end
