043c5b48bb33f4a0f681b1c3af9c176f1da56027
Abdullah2213565 <dulakhan024@gmail.com>
2022-11-21 11:31:09 +0100

    Add routes to client side

--- app/javascript/components/greeting.js
+++ app/javascript/components/greeting.js
@@ -1,9 +1,28 @@
-import React from 'react'
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
 
-const greeting = () => {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Greeting from "./components/Greeting";
+
+function App() {
+  return (<h1>Hello World!</h1>);
   return (
-    <div>greeting</div>
-  )
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<div>Static page</div>}/>
+        <Route path="/greetings" element={<Greeting />}/>
+      </Routes>
+    </div>
+  );
 }
 
-export default greeting;
+ReactDOM.render(
+  <App/>,
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
