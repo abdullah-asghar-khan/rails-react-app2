897b158d1c8225eb1ff6b56e750498fee3197a67
Abdullah2213565 <dulakhan024@gmail.com>
2022-11-21 16:21:07 +0100

    Trying to fix error

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_133702) do
+ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "greetings", force: :cascade do |t|
-    t.string "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 end
